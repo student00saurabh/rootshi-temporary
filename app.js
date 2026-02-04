9100a8486a40507c4f12de567d51618e64715dff
Saurabh Mishra <student001599@gmail.com>
2026-02-05 00:42:10 +0530

    app setting

--- app.js
+++ app.js
@@ -142,6 +142,10 @@ app.use((req, res, next) => {
   next();
 });
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
+
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] }),
