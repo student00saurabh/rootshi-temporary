d9cb753b6758593e7ad1a699fd74b86347bff613
Saurabh Mishra <student001599@gmail.com>
2026-02-05 11:23:02 +0530

    app_v1 version lounched

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,21 @@
-self.addEventListener("install", () => {
-  console.log("SW installed");
+const CACHE_NAME = "app-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))),
+      ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request)),
+  );
+});
